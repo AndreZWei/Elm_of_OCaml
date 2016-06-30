fee8dcf81743f62ab2f283bd741d5ef42324f49c
AndreZWei <zhangyang.wei2013@gmail.com>
2016-06-30 14:21:42 -0400

    Decode Partially implemented

--- decode.ml
+++ decode.ml
@@ -5,12 +5,58 @@ type 'a decoder = Decoder
 type value = Encode.value
 
 let decodePrimitive tag =
-  let argList = toList tag
-  in
-  callFun "_elm_lang$core$Native_Json.decodePrimitive" argList
-	       
+  	let argList = toList tag
+  	in
+  	callFun "_elm_lang$core$Native_Json.decodePrimitive" argList
+
+let decodeString decoder s = 
+	let argList = toList2 decoder s
+	in
+	callFun "_elm_lang$Native_Json.runOnString" argList
+
+let decodeValue decoder v = 
+	let argList = toList2 decoder v
+	in
+	callFun "_elm_lang$Native_Json.run" argList
+
 let jsonstring = 
-  decodePrimitive "string"
+  	decodePrimitive "string"
+
+let jsonint = 
+	decodePrimitive "int"
+
+let jsonfloat = 
+	decodePrimitive "float"
 
 let jsonbool =
-  decodePrimitive "bool"
+  	decodePrimitive "bool"
+
+let jsonlist decoder = 
+	let argList = toList2 "list" decoder
+    in
+    callFun "_elm_lang$Native_Json.decodeContainer" argList
+
+let jsonarray decoder = 
+	let argList = toList2 "array" decoder
+    in
+    callFun "_elm_lang$Native_Json.decodeContainer" argList
+
+let jsontuple1 f decoder =
+	let argList = toList2 f decoder
+	in
+	callFun "_elm_lang$Native_Json.decodeTuple1" argList
+
+let jsontuple2 f decoder1 decoder2 =
+	let argList = toList3 f decoder1 decoder2
+	in
+	callFun "_elm_lang$Native_Json.decodeTuple2" argList
+
+let jsontuple3 f decoder1 decoder2 decoder3 =
+	let argList = toList4 f decoder1 decoder2 decoder3
+	in
+	callFun "_elm_lang$Native_Json.decodeTuple3" argList
+
+let jsontuple4 f decoder1 decoder2 decoder3 decoder4 =
+	let argList = toList5 f decoder1 decoder2 decoder3 decoder4
+	in
+	callFun "_elm_lang$Native_Json.decodeTuple4" argList
--- decode.mli
+++ decode.mli
@@ -2,6 +2,28 @@ type 'a decoder
 
 type value = Encode.value
 
+val decodeString: 'a decoder -> string -> 'a
+
+val decodeValue: 'a decoder -> value -> 'a
+
 val jsonstring : string decoder
 
+val jsonint: int decoder
+
+val jsonfloat: float decoder
+
 val jsonbool : bool decoder
+
+val jsonlist: 'a decoder -> ('a list) decoder
+
+val jsonarray: 'a decoder -> ('a array) decoder
+
+val jsontuple1: ('a -> value) -> 'a decoder -> value decoder
+
+val jsontuple2: ('a -> 'b -> value) -> 'a decoder -> 'b decoder -> value decoder
+
+val jsontuple3: ('a -> 'b -> 'c -> value) -> 'a decoder -> 'b decoder -> 'c decoder
+-> value decoder
+
+val jsontuple4: ('a -> 'b -> 'c -> 'd -> value) -> 'a decoder -> 'b decoder -> 
+'c decoder -> 'd decoder -> value decoder 
