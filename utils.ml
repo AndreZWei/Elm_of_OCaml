c9d4b66171c168697e46aaa371a85610965ade6b
AndreZWei <zhangyang.wei2013@gmail.com>
2016-06-30 14:21:10 -0400

    Added toList5

--- utils.ml
+++ utils.ml
@@ -9,6 +9,9 @@ let toList3 a b c =
     
 let toList4 a b c d =
   [|Js.Unsafe.inject a; Js.Unsafe.inject b; Js.Unsafe.inject c; Js.Unsafe.inject d|]
+
+let toList5 a b c d e =
+  [|Js.Unsafe.inject a; Js.Unsafe.inject b; Js.Unsafe.inject c; Js.Unsafe.inject d; Js.Unsafe.inject e|]
     
 let callFun funName args =
   Js.Unsafe.fun_call (Js.Unsafe.js_expr funName) args
--- utils.mli
+++ utils.mli
@@ -1,9 +1,11 @@
 val toList : 'a -> Js.Unsafe.any array
 
-val toList2 : 'a ->'b -> Js.Unsafe.any array
+val toList2 : 'a -> 'b -> Js.Unsafe.any array
 
-val toList3: 'a ->'b -> 'c -> Js.Unsafe.any array
+val toList3: 'a -> 'b -> 'c -> Js.Unsafe.any array
 
-val toList4: 'a ->'b -> 'c -> 'd -> Js.Unsafe.any array
+val toList4: 'a -> 'b -> 'c -> 'd -> Js.Unsafe.any array
+
+val toList5: 'a -> 'b -> 'c -> 'd -> 'e -> Js.Unsafe.any array
 
 val callFun: string ->Js.Unsafe.any array -> 'c
