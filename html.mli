bef77acdc861c5914e00d75210a8dfd63ab701ae
AndreZWei <zhangyang.wei2013@gmail.com>
2016-07-01 12:02:00 -0400

    Indentation fixed

--- html.mli
+++ html.mli
@@ -1,140 +1,140 @@
-	open Decode
-	open VirtualDom
-	open Platform
-	open Platform.Cmd 
-	open Platform.Sub 
+open Decode
+open VirtualDom
+open Platform
+open Platform.Cmd 
+open Platform.Sub 
 
 
-	type 'msg html
+type 'msg html
 	
-	type 'msg attribute
+type 'msg attribute
 
 	
-	(* Custom Nodes *)
+(* Custom Nodes *)
 	
-	val text: string -> 'msg html 
+val text: string -> 'msg html 
 	
-	val node: string -> ('msg attribute) list -> ('msg html) list -> ('msg html)
+val node: string -> ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 	
-	(* Headers *)
+(* Headers *)
 	
-	val h1: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h1: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val h2: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h2: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val h3: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h3: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val h4: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h4: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val h5: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h5: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val h6: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val h6: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 
 	
-	(* Grouping Content *)
+(* Grouping Content *)
 	
-	val div: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val div: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val p: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val p: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val hr: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val hr: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val pre: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val pre: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 	
-	val blockquote: ('msg attribute) list -> ('msg html) list -> ('msg html) 
+val blockquote: ('msg attribute) list -> ('msg html) list -> ('msg html) 
 
 	
-	(* Text *)
+(* Text *)
 	
-	val span: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val span: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val a: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val a: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val code: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val code: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val em: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val em: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val strong: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val strong: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val i: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val i: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val b: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val b: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val u: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val u: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val sub: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val sub: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val sup: ('msg attribute) list -> ('msg html) list -> ('msg html)
-	
-	val br: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val sup: ('msg attribute) list -> ('msg html) list -> ('msg html)
+
+val br: ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 
-	(* Lists *)
+(* Lists *)
 	
-	val ol: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val ol: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val ul: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val ul: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val li: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val li: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val dl: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val dl: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val dt: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val dt: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val dd: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val dd: ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 	
-	(* Embedded Content *)
+(* Embedded Content *)
 	
-	val img: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val img: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val iframe: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val iframe: ('msg attribute) list -> ('msg html) list -> ('msg html)
+
+val canvas: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val canvas: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val svg: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val svg: ('msg attribute) list -> ('msg html) list -> ('msg html)
-	
-	val math: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val math: ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 
-	(* Inputs *)
+(* Inputs *)
 	
-	val form: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val form: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val input: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val input: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val textarea: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val textarea: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val button: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val button: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val select: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val select: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val option: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val option: ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 
-	(* Sections *)
+(* Sections *)
 	
-	val section: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val section: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val nav: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val nav: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val article: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val article: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val aside: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val aside: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val header: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val header: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val footer: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val footer: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val address: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val address: ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val main': ('msg attribute) list -> ('msg html) list -> ('msg html)
+val main': ('msg attribute) list -> ('msg html) list -> ('msg html)
 	
-	val body: ('msg attribute) list -> ('msg html) list -> ('msg html)
+val body: ('msg attribute) list -> ('msg html) list -> ('msg html)
 
 
-	(* Html App *)
+(* Html App *)
 
 module App: sig
 
