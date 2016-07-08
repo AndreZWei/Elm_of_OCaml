599848523dfa5df595dbe356d4414827f098c994
AndreZWei <zhangyang.wei2013@gmail.com>
2016-07-08 11:26:01 -0400

    button test

--- test.ml
+++ test.ml
@@ -1,10 +1,22 @@
 open Html
 open Wrapper
+open Html.Attributes
+open Html.Events
+open Svg
 
 type msg = Increment | Decrement
-       
+ 
+type model = int 
+
+let model = 0
+
+let update msg model = 
+	match msg with 
+	| Increment -> model + 1
+	| Decrement -> model - 1
+
 let main : (msg html)
-  = div [] [text "AAAA"]
+  = button [onClick Increment] [ text "hello"]
 	   
 let () =
   kickoff main
