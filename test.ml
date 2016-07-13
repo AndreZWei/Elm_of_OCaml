c7d88c8eecf18f5a9977d2c318aec5f8b030e6d2
AndreZWei <weizb@cs01mds260stma.bc.edu>
2016-07-13 14:08:41 -0400

    New test with program

--- test.ml
+++ test.ml
@@ -1,17 +1,34 @@
 open Html
 open Wrapper
+open Html.Attributes
 open Html
 open Html.Events
+open Svg
 open Html.Attributes
 open Html.App
-
+  
+type msg = Increment | Decrement
+  
 type model = int 
+  
+let model = 0
+  
+let update msg model = 
+  match msg with 
+  | Increment -> model + 1
+  | Decrement -> model - 1
+  
+let main : (msg html)
+  = button [onClick Increment] [ text "hello"]
+let view model =   
+ div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    ; div [] [ text (string_of_int model) ]
+    ; button [ onClick Increment ] [ text "+" ]
+    ]
+ 
+let main = beginnerProgram { model = 0; update = update; view = view}
+  	   
 
-
-
-type msg = int
-
-let (main: msg html) = button [] [text "Hello World"]
-	   
 let () =
-  kickoff main
+   kickoff main
