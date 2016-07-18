5ee05bdcecf4fbd01dc24e87c04716559e7c2148
AndreZWei <zhangyang.wei2013@gmail.com>
2016-07-18 11:44:53 -0400

    Textarea

--- test.ml
+++ test.ml
@@ -4,10 +4,9 @@ open Html.Attributes
 open Html
 open Html.Events
 open Svg
-open Html.Attributes
 open Html.App
   
-type msg = Increment | Decrement
+type msg = Increment | Decrement | ChangeModel of string
   
 type model = int 
   
@@ -17,12 +16,16 @@ let update msg model =
   match msg with 
   | Increment -> model + 1
   | Decrement -> model - 1
+  | ChangeModel number -> int_of_string number
+  
+let changemodel number = ChangeModel number
   
 let view model =   
  div []
     [ button [ onClick Decrement ] [ text "-" ]
     ; div [] [ text (string_of_int model) ]
     ; button [ onClick Increment ] [ text "+" ]
+    ; textarea [ onInput changemodel ] []
     ]
  
 let main = beginnerProgram { model = model; update = update; view = view}
